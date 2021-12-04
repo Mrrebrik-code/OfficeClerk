d37a357f994ccab7c7578ba809e2e10d341b6b4e
Mrrebrik <itibsoft@gmail.com>
2021-12-04 21:04:08 +0700

    Shop load profucts; Add Floder Resources

--- Assets/Scripts/Shop/Product.cs
+++ Assets/Scripts/Shop/Product.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "product", menuName = "Shop/Product")]
 public class Product : ScriptableObject
 {
 	public int Id;
 	public TypeProduct Type;
-	
+	public string Name;
+	public string Description;
+	public int Price;
+
+	public int LevelUp;
 }
 
--- Assets/Scripts/Shop/ShopHandler.cs
+++ Assets/Scripts/Shop/ShopHandler.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 public class ShopHandler : MonoBehaviour
 {
+	private List<Product> _products = new List<Product>();
 
+	private void Awake()
+	{
+		LoadProducts();
+	}
+
+	private bool LoadProducts()
+	{
+		_products = Resources.LoadAll<Product>("Products").ToList();
+		return _products != null;
+	}
 }
