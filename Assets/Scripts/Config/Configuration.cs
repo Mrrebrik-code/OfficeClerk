785e22e33681f803250881857d55c9a0a9a76b94
Mrrebrik <itibsoft@gmail.com>
2021-12-19 18:42:37 +0700

    Create system configuration

---
+++ Assets/Scripts/Config/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Config", menuName = "SO/Config")]
+public class Configuration : ScriptableObject
+{
+	[Header("Игровая валюта:")]
+	public int CountCookiesStart;
+	public int CountDiamondStart;
+
+	[Header("Баланс игры:"), Space]
+	[Tooltip("[ЖАЖДА] Значение, которое отнимается у рабочих, когда они работают")]
+	public int ThirstValue;
+	[Tooltip("[ГОЛОД] Значение, которое отнимается у рабочих, когда они работают")]
+	public int HungryValue;
+	[Tooltip("[УСТАЛОСТЬ] Значение, которое отнимается у рабочих, когда они работают")]
+	public int DreamValue;
+
+}
---
+++ Assets/Scripts/Config/Configurator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class Configurator : SingletonMono<Configurator>
+{
+	public Configuration Data{ get; private set; }
+
+	public override void Awake()
+	{
+		IsDontDestroyOnLoad = true;
+		base.Awake();
+		Data = Resources.LoadAll<Configuration>("Config")[0];
+	}
+}
