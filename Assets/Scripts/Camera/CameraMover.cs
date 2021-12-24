239d8079721e692ba5e74b40e59a359809dff6c4
Mrrebrik <itibsoft@gmail.com>
2021-12-24 21:52:13 +0700

    Camera mover clamp position

--- Assets/Scripts/Camera/CameraMover.cs
+++ Assets/Scripts/Camera/CameraMover.cs
@@ -5,6 +5,10 @@ public class CameraMover : MonoBehaviour
 	private Transform _transform;
 	private float _xPosition, _zPosition;
 	private float _yPositionCamera = 7f;
+	[SerializeField] private float _minX;
+	[SerializeField] private float _maxX;
+	[SerializeField] private float _minZ;
+	[SerializeField] private float _maxZ;
 	private void Start()
 	{
 		_transform = GetComponent<Transform>();
@@ -17,6 +21,8 @@ public class CameraMover : MonoBehaviour
 		_zPosition += Input.GetAxis("Vertical");
 
 		_yPositionCamera = Mathf.Clamp(_yPositionCamera, Configurator.Instance.Data.MinPositionCamera, Configurator.Instance.Data.MaxPositionCamera);
-		_transform.localPosition = new Vector3(-_xPosition * Configurator.Instance.Data.SpeedMoveCamera, _yPositionCamera, -_zPosition * Configurator.Instance.Data.SpeedMoveCamera);
+		_xPosition = Mathf.Clamp(_xPosition , _minX, _maxX);
+		_zPosition = Mathf.Clamp(_zPosition , _minZ, _maxZ);
+		_transform.localPosition = new Vector3(-_xPosition * Configurator.Instance.Data.SpeedMoveCamera , _yPositionCamera, -_zPosition * Configurator.Instance.Data.SpeedMoveCamera);
 	}
 }
